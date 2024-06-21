a52cf6cf01e8025682ebc87d65c5198f7cf75639
eswlo <sl3923@columbia.edu>
2024-06-21 12:55:57 -0700

    save generated table as html

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ customer_index = 1
 item_index = 2
 quantity_index = 3
 instruction = "Enter the name of the csv file stored in the folder, or press enter to quit: "
+output_html_table = "table_of_orders.html"
 
 if __name__ == '__main__':
     filename = input(instruction)
@@ -17,7 +18,22 @@ if __name__ == '__main__':
             purchase_dict_unorg = get_unorg_dict(purchase_list, customer_index, item_index, quantity_index)
             purchase_dict_org = get_org_dict(purchase_dict_unorg)
             rowID = get_row_index(purchase_dict_org)
-            print(tabulate(purchase_dict_org, headers="keys", tablefmt="fancy_grid", showindex=rowID))
+
+            # format the organized purchase dict into tabular data  
+            table = tabulate(purchase_dict_org, headers="keys", tablefmt="fancy_grid", showindex=rowID)
+            
+            # extract html code from the tabular data
+            table_html = tabulate(purchase_dict_org, headers="keys", tablefmt="html", showindex=rowID)
+
+            # print the table to console
+            print(table)
+            
+            # write the table html to file
+            with open(output_html_table, 'w') as f:
+                f.write(table_html)
+                # f.close
+            
+            # continue to the next file to be processed
             filename = input(instruction)
         else:
             print("No file was found in the folder.")
